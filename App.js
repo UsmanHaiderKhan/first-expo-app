466a1cf08d8a6c4b657fbfe237f5cdf27f12e551
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-11-16 17:26:58 +0500

    🎉 Add : Goal noting Application with scroll view list

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoal, setCourseGoal] = useState([]);
+
+  const goalInputHandler = (enteredText)=>{
+     setEnteredGoal(enteredText);
+  }
+  const addGoalHandler = ()=>{
+    setCourseGoal(currentGoal => [...currentGoal, enteredGoal]);  
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.mainWrapper}>
+        <TextInput placeholder='Course Goals' style={styles.inputText} onChangeText={goalInputHandler} value={enteredGoal}></TextInput>
+        <Button title='Add Your Goal' style={styles.inputButton} onPress={addGoalHandler}></Button>
+      </View>
+      <ScrollView>
+      {courseGoal.map(goal => 
+        <View key={goal} style={styles.listItem}>
+          <Text>{goal}</Text>
+        </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    marginTop:40,
+    padding:20
+    },
+    mainWrapper:{
+      flexDirection:'row', 
+      alignItems:'center',
+      justifyContent:'space-between'
+    },
+    inputText:{
+      width:'60%',
+      borderBottomWidth:1,
+      borderBottomColor:'black',
+    },
+    inputButton: {
+      marginTop:15
+    },
+    listItem:{
+      padding:10,
+      borderWidth:1,
+      marginVertical:10,
+      borderColor:'black',
+      backgroundColor:'#ccc',
+    }
+
 });
