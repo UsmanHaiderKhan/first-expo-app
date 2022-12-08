3ba5221a3a8c6f2120e01084f0a3ab7ef1793422
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-12-08 19:12:46 +0500

    ⚒️ Fix : the goal items and add lietItem styling

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,20 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text,StyleSheet} from 'react-native';
 
-const GoalItem = prop =>{
+const GoalItem = props =>{
     return (
         <View style={styles.listItem.value}>
-          <Text>{dataItem.item}</Text>
+          <Text>{props.Text }</Text>
         </View>
-    )
+    );
 }
+const styles = StyleSheet.create({
+    listItem: {
+        padding: 10,
+        borderWidth: 1,
+        marginVertical: 10,
+        borderColor: 'black',
+        backgroundColor: '#ccc',
+      }
+});
 export default GoalItem;
