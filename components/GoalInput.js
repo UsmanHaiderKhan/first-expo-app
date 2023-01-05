1b9a4035dacd63e027bb2c24cc10550984cea5f4
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-01-05 14:52:38 +0500

    🎉 add : New cancel button and align with add goal button

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -6,11 +6,23 @@ const GoalInput = props =>{
     const goalInputHandler = enteredText => {
         setEnteredGoal(enteredText);
     }
+    addGoalHandler = ()=>{
+      props.addGoalHandler(enteredGoal);
+      setEnteredGoal('');
+    }
     return(
       <Modal visible={props.visible} animationType="slide">
         <View style={styles.mainWrapper}>
           <TextInput placeholder='Course Goals' style={styles.inputText} onChangeText={goalInputHandler} value={enteredGoal}></TextInput>
-          <Button title='Add Your Goal' style={styles.inputButton} onPress={props.onAddGoal.bind(this, enteredGoal)}></Button>
+          <View style={styles.buttonAlignment}>
+            <View style={styles.button}>
+               <Button title='CANCEL' color="red" onPress={props.onCancel}/>
+            </View>
+            <View style={styles.button}>
+               <Button title='Add Your Goal' style={styles.inputButton} onPress={props.onAddGoal.bind(this, enteredGoal)}></Button>
+            </View>
+          </View>
+          
         </View>
       </Modal>
     
@@ -18,17 +30,26 @@ const GoalInput = props =>{
 }
 const styles = StyleSheet.create({
     mainWrapper: {
-        flexDirection: 'row',
+        flex:1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'center'
       }, 
     inputText: {
-        width: '60%',
+        width: '80%',
         borderBottomWidth: 1,
+        marginBottom:10,
         borderBottomColor: 'black',
       },
       inputButton: {
         marginTop: 15
       },
+      buttonAlignment : {
+        width:'80%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      button:{
+        width: '45%'
+      }
 });
  export default GoalInput;
